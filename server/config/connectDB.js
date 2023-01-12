51af52a49e9a1e6f596cec844fa89bd3affa8407
Nathan Teissier <nmteissier@gmail.com>
2023-01-12 12:03:36 +0100

    test render

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  `${process.env.PSQL_POSTGRES}://${process.env.PSQL_USERNAME}:${process.env.PSQL_PASSWORD}@${process.env.PSQL_HOST}:${process.env.PSQL_PORT}/${process.env.PSQL_DATABASE_NAME}`
+  // `${process.env.PSQL_POSTGRES}://${process.env.PSQL_USERNAME}:${process.env.PSQL_PASSWORD}@${process.env.PSQL_HOST}:${process.env.PSQL_PORT}/${process.env.PSQL_DATABASE_NAME}`
+  'postgres://luigiv:qK3jrtrUpvylOgQyayOjrt3d9oq0Pyk0@dpg-cevu2marrk0eqcoehheg-a.frankfurt-postgres.render.com:5432/bank_one_zswa'
 );
 
 const connectDB = async () => {
